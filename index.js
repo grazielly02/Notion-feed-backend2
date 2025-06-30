ca9ba86d33bf2ee220bcd6fd4be0017093ee2c78
Grazielly  Santana <43781255-suportegraziell@users.noreply.replit.com>
2025-06-30 19:49:49 +0000

    Fix: restaurar index.html para versão estável

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function queryDatabase(token, databaseId) {
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "widget.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/config", (req, res) => {
