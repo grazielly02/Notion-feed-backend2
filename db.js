d4aab0f115a821c01d25dff1de13c1f89f5615b0
grazielly02 <suportegraziellyoliveira@gmail.com>
2025-07-24 20:46:07 -0300

    Para supabase db.js

--- db.js
+++ db.js
@@ -10,18 +10,26 @@ module.exports = {
   query: (text, params) => pool.query(text, params),
 
   getConfig: async (clientId) => {
-    const res = await pool.query('SELECT * FROM configs WHERE clientId = $1', [clientId]);
-    console.log("Resultado da busca por config:", res.rows[0]);  // Adicione este log
+    const trimmedClientId = clientId.trim();
+    const res = await pool.query(
+      'SELECT * FROM configs WHERE "clientId" = $1',
+      [trimmedClientId]
+    );
+    console.log("Resultado da busca por config:", res.rows[0]);
     return res.rows[0];
   },
 
   saveConfig: async (clientId, token, databaseId) => {
+    const trimmedClientId = clientId.trim();
+    const trimmedToken = token.trim();
+    const trimmedDatabaseId = databaseId.trim();
+
     await pool.query(
-      `INSERT INTO configs (clientId, token, databaseId)
+      `INSERT INTO configs ("clientId", token, "databaseId")
        VALUES ($1, $2, $3)
-       ON CONFLICT (clientId)
-       DO UPDATE SET token = EXCLUDED.token, databaseId = EXCLUDED.databaseId`,
-      [clientId, token, databaseId]
+       ON CONFLICT ("clientId")
+       DO UPDATE SET token = EXCLUDED.token, "databaseId" = EXCLUDED."databaseId"`,
+      [trimmedClientId, trimmedToken, trimmedDatabaseId]
     );
   },
 };
